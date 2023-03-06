cc92fda6cac54fbfd07836d49204a519dc7007e5
TLNegrao <89546780+TLNegrao@users.noreply.github.com>
2023-03-05 22:50:27 -0300

    trabalho materialUiEssentials

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import './index.css';
 import './App.css';
+import { Button, Typography } from '@mui/material';
+import {styled} from '@mui/material/styles';
+
+
 
 function App() {
+
+  const MyCustomButton = styled(Button)(({theme}) => ({
+    padding: theme.spacing(2)
+  }))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Typography variant='myVariant'>Outra coisa?</Typography>
+        <Typography sx={{color: 'myCustomColor.superLight'}}>Mai uma outra coisa?</Typography>
+        <MyCustomButton variant='contained'>Olá Mundo!!!!</MyCustomButton>
+
     </div>
   );
 }
